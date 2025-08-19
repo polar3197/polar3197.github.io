d7d5779cc9f136024d670e9ca39de5a55c497fb5
polar3197 <56515625+polar3197@users.noreply.github.com>
2025-08-19 09:46:25 +0000

    Deploying to gh-pages from @ polar3197/polar3197.github.io@236d1b2a77294f6b5a61dc2a25358f6d5ac65f85 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/map/";
           },
+        },{id: "nav-map",
+          title: "map",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/map/";
+          },
         },{id: "post-nowhere-notime",
       
         title: "Nowhere, Notime",
