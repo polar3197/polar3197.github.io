125ab895f9ffa0843049c386c7114bcf334ede38
polar3197 <56515625+polar3197@users.noreply.github.com>
2025-12-12 20:24:06 +0000

    Deploying to gh-pages from @ polar3197/polar3197.github.io@ecb2a263d942d416901eaf5852b85c7b4566f77f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
@@ -22,13 +29,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/gallery/";
-          },
-        },{id: "nav-creative-writing",
-          title: "creative writing",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/creative/";
           },
         },{id: "post-youth-is-wasted-time",
       
